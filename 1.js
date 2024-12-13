701440b24e4c0ee0f5256eed34c3743d00a2175e
jdsj666 <19524359553@163.com>
2024-12-13 18:16:31 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,29 @@
+$(()=>{
+
+    //渲染导航栏：
+    $(".banner").append(`
+
+        <div class="nav">
+        <h2>Mozart-L'opera Rock</h2>
+        <div class="as">
+        <a href=""C:\Users\ASUS\Desktop\网页\摇滚莫扎特\pages\Mozart-L'opera Rock.html">Brief introduction</a>
+        <a href=""C:\Users\ASUS\Desktop\网页\摇滚莫扎特\pages\Synopsis.html">Synopsis</a>
+        <a href=""C:\Users\ASUS\Desktop\网页\摇滚莫扎特\pages\Character introduction.html">Character introduction</a>
+        <a href=""C:\Users\ASUS\Desktop\网页\摇滚莫扎特\pages\Song snippets.html">Song snippets</a>
+        <a href=""C:\Users\ASUS\Desktop\网页\摇滚莫扎特\pages\login.html">Enroll and login</a>
+        </div>
+    </div>
+ `)
+
+ //渲染footer部分：
+ $(".footer").append(`
+    
+    
+    <h2>Mozart-L'opera Rock</h2>
+
+    <div class="copyright">
+        Copyright 2024 Jie Shen.AllRightsReserved
+    </div>
+ 
+    `)
+})
---
+++ 2.js
@@ -0,0 +1,29 @@
+$(()=>{
+
+    //渲染导航栏：
+    $(".banner").append(`
+
+        <div class="nav">
+        <h2>Mozart-L'opera Rock</h2>
+        <div class="as">
+         <a href="Mozart-L'opera Rock.html">Brief introduction</a>
+        <a href="Synopsis.html">Synopsis</a>
+        <a href="Character introduction.html">Character introduction</a>
+       
+        <a href="login.html">Enroll and login</a>
+        </div>
+    </div>
+ `)
+
+ //渲染footer部分：
+ $(".footer").append(`
+    
+    
+    <h2>Mozart-L'opera Rock</h2>
+
+    <div class="copyright">
+        Copyright 2024 Jie Shen.AllRightsReserved
+        </div>
+ 
+    `)
+})
